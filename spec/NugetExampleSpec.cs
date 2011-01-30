33d84df57675b4d34e0ff450c587201422638fec
remi <remi@remitaylor.com>
2011-01-30 02:19:31 -0700

    new Service(root).Metadata.EntityTypes.  Gets Properties and Keys for each EntityType.

--- spec/NugetExampleSpec.cs
+++ spec/NugetExampleSpec.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using Requestoring;
 using EasyOData;
 using NUnit.Framework;
@@ -16,6 +17,7 @@ namespace EasyOData.Specs {
 		public void Before() {
 			base.Before();
 			FakeResponse(NuGetServiceRoot, "NuGet", "root.xml");
+			FakeResponse(NuGetServiceRoot + "$metadata", "NuGet", "metadata.xml");
 		}
 
 		[Test]
@@ -25,13 +27,76 @@ namespace EasyOData.Specs {
 			service.CollectionNames.ShouldContain("Screenshots");
 		}
 
-		[Test][Ignore]
+		[Test]
 		public void can_get_collections() {
 			service.Collections.Count.ShouldEqual(2);
+
 			service.Collections.First().Name.ShouldEqual("Packages");
 			service.Collections.First().Href.ShouldEqual("Packages");
+			service.Collections.First().Service.ShouldEqual(service);
+
 			service.Collections.Last().Name.ShouldEqual("Screenshots");
 			service.Collections.Last().Href.ShouldEqual("Screenshots");
+			service.Collections.Last().Service.ShouldEqual(service);
+		}
+
+		[Test]
+		public void can_get_metadata() {
+			var metadata = service.Metadata;
+
+			// <EntityType>
+			metadata.EntityTypes.Count.ShouldEqual(2);
+
+			metadata.EntityTypeNames.Count.ShouldEqual(2);
+			metadata.EntityTypeNames.ShouldContain("PublishedPackage");
+			metadata.EntityTypeNames.ShouldContain("PublishedScreenshot");
+
+			var package = metadata.EntityTypes["PublishedPackage"];
+
+			// // <Property>
+			package.Properties.Count.ShouldEqual(32);
+
+			new List<string> {
+				"Id", "Version", "Title", "Authors", "PackageType", "Summary", "Description", "Copyright",
+				"PackageHashAlgorithm", "PackageHash", "PackageSize", "Price", "RequireLicenseAcceptance", 
+				"IsLatestVersion", "VersionRating", "VersionRatingsCount", "VersionDownloadCount", "Created",
+				"LastUpdated", "Published", "ExternalPackageUrl", "ProjectUrl", "LicenseUrl", "IconUrl", "Rating",
+				"RatingsCount", "DownloadCount", "Categories", "Tags", "Dependencies", "ReportAbuseUrl", "GalleryDetailsUrl"
+			}.ForEach(s => package.PropertyNames.ShouldContain(s));
+
+			package.Properties["Id"].Type.ToString().ShouldEqual("Edm.String");
+			package.Properties["Id"].IsNullable.ShouldBeFalse();
+
+			package.Properties["Authors"].Type.ToString().ShouldEqual("Edm.String");
+			package.Properties["Authors"].IsNullable.ShouldBeTrue();
+
+			package.Properties["PackageSize"].Type.ToString().ShouldEqual("Edm.Int64");
+			package.Properties["PackageSize"].IsNullable.ShouldBeFalse();
+
+			package.Properties["IsLatestVersion"].Type.ToString().ShouldEqual("Edm.Boolean");
+			package.Properties["IsLatestVersion"].IsNullable.ShouldBeFalse();
+
+			// <Key>
+			package.Keys.Count.ShouldEqual(2);
+
+			package.Keys.First().Name.ShouldEqual("Id");
+			package.Keys.First().Type.ToString().ShouldEqual("Edm.String");
+			package.Keys.First().IsNullable.ShouldBeFalse();
+
+			package.Keys.Last().Name.ShouldEqual("Version");
+			package.Keys.Last().Type.ToString().ShouldEqual("Edm.String");
+			package.Keys.Last().IsNullable.ShouldBeFalse();
+
+			// var screenshot = metadata.EntityTypes["PublishedScreenshot"];
+			// ...
+
+			// <Association>
+			// metadata.Associations.Count.ShouldEqual(1);
+			// ...
+
+			// <EntityContainer>
+			// metadata.EntityContainers.Count.ShouldEqual(1);
+			// ...
 		}
 	}
 }
--- src/EasyOData.cs
+++ src/EasyOData.cs
@@ -11,10 +11,129 @@ using Requestoring;
 
 namespace EasyOData {
 
+	public static class XmlParsing {
+
+		public static IEnumerable<XmlNode> GetElementsByTagName(this XmlNode node, string tagName) {
+			var nodes = new List<XmlNode>();
+			foreach (XmlNode child in node.ChildNodes)
+				if (child.Name == tagName)
+					nodes.Add(child);
+			return nodes;
+		}
+
+		public static string Attr(this XmlNode node, string attributeName) {
+			if (node.Attributes[attributeName] != null)
+				return node.Attributes[attributeName].Value;
+			else
+				return null;
+		}
+
+		public static Property ToProperty(this XmlNode node) {
+			return new Property {
+				Name       = node.Attr("Name"),
+				TypeName   = node.Attr("Type"),
+				IsNullable = bool.Parse(node.Attr("Nullable"))
+			};
+		}
+
+		public static EntityType ToEntityType(this XmlNode node, Metadata metadata) {
+			var type = new EntityType();
+
+			// Name
+			type.Name = node.Attr("Name");
+
+			// Properties
+			foreach (XmlNode propertyNode in node.GetElementsByTagName("Property"))
+				type.Properties.Add(propertyNode.ToProperty());
+
+			// Keys
+			foreach (XmlNode keyNode in node.GetElementsByTagName("Key"))
+				foreach (XmlNode propertyRef in keyNode.GetElementsByTagName("PropertyRef"))
+					type.Properties[propertyRef.Attr("Name")].IsKey = true;
+
+			// Associations
+
+			return type;
+		}
+
+		public static Collection ToCollection(this XmlNode node, Service service) {
+			return new Collection {
+				Name    = node["atom:title"].InnerText,
+				Href    = node.Attr("href"),
+				Service = service
+			};
+		}
+
+	}
+
 	public class Collection {
 		public Service Service { get; set; }
 		public string Name     { get; set; }
 		public string Href     { get; set; }
+	}
+
+	public class Property {
+		public string Name { get; set; }
+		public string TypeName { get; set; } // TODO this should eventually just get { EdmType.Name }
+		public bool IsNullable { get; set; }
+		public bool IsKey { get; set; }
+
+		public string Type { get { return TypeName; } } // this should be the entity type, eventually
+	}
+
+	public class PropertyList : List<Property> {
+		public PropertyList() {}
+		public PropertyList(IEnumerable<Property> properties) {
+			AddRange(properties);
+		}
+
+		public Property this[string name] {
+			get { return this.FirstOrDefault(property => property.Name == name); }
+		}
+	}
+
+	public class EntityType {
+		public EntityType() {
+			Properties = new PropertyList();
+		}
+
+		public string Name { get; set; }
+		public PropertyList Properties { get; set; }
+
+		public List<string> PropertyNames { get { return Properties.Select(property => property.Name).ToList(); } }
+
+		public PropertyList Keys { get { return new PropertyList(Properties.Where(p => p.IsKey)); } }
+	}
+
+	public class EntityTypeList : List<EntityType> {
+		public EntityType this[string name] {
+			get { return this.FirstOrDefault(type => type.Name == name); }
+		}
+	}
+
+	public class Metadata {
+
+		public Metadata(){}
+		public Metadata(Service service) {
+			Service = service;
+		}
+
+		public Service Service { get; set; }
+
+		public List<string> EntityTypeNames { get { return EntityTypes.Select(t => t.Name).ToList(); } }
+
+		public EntityTypeList EntityTypes {
+			get {
+				var types = new EntityTypeList();
+				foreach (XmlNode node in Service.GetXml("$metadata").GetElementsByTagName("EntityType"))
+					types.Add(node.ToEntityType(this));
+				return types;
+			}
+		}
+
+		// public List<Property> Properties {
+		// 	
+		// }
 	}
 
 	/// <summary>Represents an OData Service</summary>
@@ -30,15 +149,14 @@ namespace EasyOData {
 		public List<Collection> Collections {
 			get {
 				var collections = new List<Collection>();
-				
 				foreach (XmlNode node in GetXml("/").GetElementsByTagName("collection"))
-					collections.Add(new Collection {
-						Name = node["atom:title"].InnerText,
-						Href = node.Attributes["href"].Value
-					});
-
+					collections.Add(node.ToCollection(this));
 				return collections;
 			}
+		}
+
+		public Metadata Metadata {
+			get { return new Metadata(this); }
 		}
 
 		public List<string> CollectionNames {
@@ -56,7 +174,7 @@ namespace EasyOData {
 			return _requestor.Get(CombineUrl(Root, path));
 		}
 
-		XmlDocument GetXml(string path) {
+		public XmlDocument GetXml(string path) {
 			return GetXmlDocumentForString(Get(path).Body);
 		}
 		#endregion
