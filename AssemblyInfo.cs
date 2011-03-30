a92cd3170a6e928dc8f86ec7279919ca19a76bdc
remi <remi@remitaylor.com>
2011-03-30 15:53:54 -0700

    Added AssemblyInfo

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: AssemblyTitle("EasyOData")]
+[assembly: AssemblyProduct("EasyOData")]
+[assembly: AssemblyDescription("Easy to use C# OData client")]
+[assembly: AssemblyCompany("remi Taylor")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
